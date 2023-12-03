ffc1ce8dfb4730a4e94806e598a53dc5cf868fdd
Terry Wambui <shikuwambui@gmail.com>
2023-12-04 01:49:16 +0300

    styled the header

---
+++ src/sections/header/data.js
@@ -0,0 +1,13 @@
+// import {AiOutlineInstagram} from 'react-icons/ai'
+import {AiOutlineTwitter} from 'react-icons/ai'
+import {AiOutlineLinkedin} from 'react-icons/ai'
+import {AiFillGithub} from 'react-icons/ai'
+import { FaHashnode } from "react-icons/fa6";
+
+const data = [
+    {id: 1, link: 'https://sophiaintech.hashnode.dev/', icon: <FaHashnode/>},
+    {id: 2, link: 'https://twitter.com/Mssophia_', icon: <AiOutlineTwitter/>},
+    {id: 3, link: 'https://www.linkedin.com/in/terry-wambui-0629b7115/', icon: <AiOutlineLinkedin/>},
+    {id: 4, link: 'https://github.com/terryyufei', icon: <AiFillGithub/>}
+]
+ export default data;
