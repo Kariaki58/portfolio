911c6f101ee1d4dc00fd1ca690c0ff02a93c1100
Terry Wambui <shikuwambui@gmail.com>
2023-12-13 09:48:02 +0300

    theme customization

---
+++ src/context/theme-context.js
@@ -0,0 +1,21 @@
+import { createContext, useContext, useReducer, useEffect } from "react";
+import themeReducer from "./themeReducer";
+
+export const ThemeContext = createContext();
+const initialThemeState = {primary: 'color-1', background: 'bg-1'}
+
+export const ThemeProvider = ({children}) => {
+    const [themeState, dispatchTheme] = useReducer(themeReducer, initialThemeState);
+
+    const themeHandler = (buttonClassName) => {
+        dispatchTheme({type: buttonClassName})
+    }
+
+
+    return <ThemeContext.Provider value={{themeState, themeHandler}}>{children}</ThemeContext.Provider>
+}
+
+// custom hook to use our theme context anywhere in the project 
+export const useThemeContext = () => {
+    return useContext(ThemeContext);
+}
