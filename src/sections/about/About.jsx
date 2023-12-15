e113115bfa242efa356510c44b3187932eb0eb0d
Terry Wambui <shikuwambui@gmail.com>
2023-12-16 02:07:41 +0300

    animate and build

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -7,7 +7,7 @@ import './about.css'
 
 const About = () => {
     return (
-        <section id='about'>
+        <section id='about' data-aos="zoom-in">
             <div className="container about__container">
                 <div className="about__left">
                     <div className="about__potrait">
@@ -33,7 +33,7 @@ const About = () => {
                         40 happy clients worlwide. I'm always motivated to do more!
                     </p>
                     <p>
-                        Hi, my name is Sophia from Nairobi, Kenya. I'm a full-stack web developer with a Certificate 
+                        Hi, my name is Terry Wambui from Nairobi, Kenya. I'm a full-stack web developer with a Certificate 
                         in Software Engineering. My top priority is to get your business online the right way, giving
                         you industry-standard design and all the functionality you need to operate smoothly online. Get in touch 
                         with the details of your project let's get started! Check out my resume below!
--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,33 +1,50 @@
-import HeaderImage from '../../assets/header.jpg';
-import data from './data';
-import './header.css';
-
+import { useEffect } from "react";
+import HeaderImage from "../../assets/header.jpg";
+import data from "./data";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "./header.css";
 
 const Header = () => {
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
+
   return (
-    <header id='header'>
+    <header id="header">
       <div className="container header__container">
-        <div className="header__profile">
-          <img src={HeaderImage} alt="Header Portrait" />
+        <div className="header__profile" data-aos="fade-in">
+          <img src={HeaderImage} alt="Header Portait" />
         </div>
-        <h3>Sophia</h3>
-        <p>
-          You are a click away from building your dream website or web app. Send me 
-          the details of your project for a modern, mobile responsive, highly
+        <h3 data-aos="fade-up">Terry Wambui</h3>
+        <p data-aos="fade-up">
+          You are a click away from building your dream website or web app. Send
+          me the details of your project for a modern, mobile responsive, highly
           performant website today!
         </p>
-        <div className="header__cta">
-          <a href="#contact" className='btn primary'>Let's Talk</a>
-          <a href="#portfolio" className='btn light'>My Work</a>
+        <div className="header__cta" data-aos="fade-up">
+          <a href="#contact" className="btn primary">
+            Let's Talk
+          </a>
+          <a href="#portfolio" className="btn light">
+            My Work
+          </a>
         </div>
         <div className="header__socials">
-          {
-            data.map(item => <a key={item.id} href={item.link} target='_blank' rel='noopener noreferrer' >{item.icon}</a>) 
-          }
+          {data.map((item) => (
+            <a
+              key={item.id}
+              href={item.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.icon}
+            </a>
+          ))}
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
