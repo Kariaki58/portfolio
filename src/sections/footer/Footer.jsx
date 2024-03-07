82ea8218f5876fe2d5949ec818b2c330054a40bc
Terry Wambui <shikuwambui@gmail.com>
2024-03-07 21:17:27 +0300

    Chore: (Added Dynamic Date)

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = () => {
       </div>
     </div>
     <div className="footer__copyright">
-      <small>2023 Terry Wambui &copy; All Rights Reserved</small>
+    <small>{new Date().getFullYear()} Terry Wambui &copy; All Rights Reserved</small>
     </div>
    </footer>
   )
